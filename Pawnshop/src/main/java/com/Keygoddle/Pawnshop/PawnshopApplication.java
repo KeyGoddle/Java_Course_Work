e012a3f2ae3676d243e224840e0dd8bab701f7cb
KeyGoddle <al_13@bk.ru>
2021-07-03 11:32:05 +0300

    HH

--- Pawnshop/src/main/java/com/Keygoddle/Pawnshop/PawnshopApplication.java
+++ Pawnshop/src/main/java/com/Keygoddle/Pawnshop/PawnshopApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PawnshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PawnshopApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(PawnshopApplication.class, args);
 	}
 
 }
